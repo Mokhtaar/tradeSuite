9bff235bb0f3ffb36d4962d0ea09ce852de30d5b
jumana <jumana1903964@miuegypt.edu.eg>
2023-12-08 05:23:51 +0200

    user register

--- src/app/Dahboard/page.js
+++ src/app/Dahboard/page.js
@@ -2,10 +2,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use server";
 
-import Link from "next/link";
-import prisma from "../../../lib/prisma";
 import MyComponent from '../components/Dashboard';
-
   
 const Mypage = (props) => {
   return <MyComponent 
