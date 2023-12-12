b07fcd06ce5e2a502df68d2eaaf46f3d7ad6da66
jumana <jumana1903964@miuegypt.edu.eg>
2023-12-12 03:26:15 +0200

    UploadDocuments

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -1,6 +1,9 @@
 "use client"
-import { Fragment, useState } from 'react'
+
 import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react';
+import Link from 'next/link';
+
 
 import {
   CalendarIcon,
@@ -35,8 +38,15 @@ function classNames(...classes) {
 }
 
 const  MyComponent =() => {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
- 
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const [currentPath, setCurrentPath] = useState('');
+
+  const handleItemClick = (path) => {
+    setCurrentPath(path);
+  };
+
 
   return (
     <>
@@ -201,20 +211,22 @@ const  MyComponent =() => {
                   <ul role="list" className="-mx-2 space-y-1">
                     {navigation.map((item) => (
                       <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'bg-gray-50 text-indigo-600'
-                              : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-                            'group flex gap-x-3 text-gray-700 rounded-md p-2 text-sm leading-6 font-semibold'
-                          )}
-                        >
+                          
+                         <a
+                         onClick={() => handleItemClick(item.href)}
+                   
+                    className={classNames(
+                      currentPath === item.href ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
+                      'group flex gap-x-3 text-gray-700 rounded-md p-2 text-sm leading-6 font-semibold'
+                    )}
+                  
+                  >
                           <item.icon
                             className={classNames(
-                              item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600',
+                              currentPath === item.href ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600',
                               'h-6 w-6 shrink-0'
                             )}
+                            onClick={() => handleItemClick(item.href)}
                             aria-hidden="true"
                           />
                           {item.name}
