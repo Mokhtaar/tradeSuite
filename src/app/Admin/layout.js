c795ce4a6c4bce228859756b88becd9ac2b2dd54
jumana <jumana1903964@miuegypt.edu.eg>
2023-12-20 04:09:54 +0200

    AdminSide

---
+++ src/app/Admin/layout.js
@@ -0,0 +1,19 @@
+
+import { Inter } from "next/font/google";
+import Example from '../components/AdminMenu';
+
+import Table1 from '../components/UserTable';
+
+export const metadata = {
+  title: "TradeSuite",
+  description: "Document Translation Hub",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <>
+    <Example/>
+    <Table1/>
+    </>
+  );
+}
