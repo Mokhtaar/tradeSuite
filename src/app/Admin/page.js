7e65d5112f7cb28f5d92e43d9c6de878ae14a2ef
Mokhtaar <mokh.first@gmail.com>
2023-12-20 15:39:04 +0200

    getusers

--- src/app/Admin/page.js
+++ src/app/Admin/page.js
@@ -1,13 +1,13 @@
-import React from 'react'
-
+import React from "react";
 import { getCurrentUser } from "../../../lib/session";
-async function Admin  ()  {
-  const user = await getCurrentUser();
-  console.log(user);
+
+import prisma from "../../../lib/prisma";
+async function Admin() {
+  const users = await prisma.user.findMany();
+  console.log(users);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      </main>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-between"></main>
+  );
 }
 
-export default Admin
+export default Admin;
