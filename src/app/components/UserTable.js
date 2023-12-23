661e424fbda2ae9c63ea5203d7d0788629601026
Mokhtaar <mokh.first@gmail.com>
2023-12-23 12:57:29 +0200

    edit

--- src/app/components/UserTable.js
+++ src/app/components/UserTable.js
@@ -1,25 +1,23 @@
-"use client"
+"use client";
 
 import React from "react";
 import prisma from "../../../lib/prisma";
 import { Update } from "../Actions/adminActions";
 
-  export default async function Table1() {
+export default async function Table1() {
+  const users = await prisma.user.findMany();
 
-    const users = await prisma.user.findMany();
-   
-  
-
-
-    return (
-        
-      <div className="px-4 sm:px-6 lg:px-8">
-         <main className="py-10 lg:pl-72">
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <main className="py-10 lg:pl-72">
         <div className="sm:flex sm:items-center">
           <div className="sm:flex-auto">
-            <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1>
+            <h1 className="text-base font-semibold leading-6 text-gray-900">
+              Users
+            </h1>
             <p className="mt-2 text-sm text-gray-700">
-              A list of all the users in your account including their name, title, email and role.
+              A list of all the users in your account including their name,
+              title, email and role.
             </p>
           </div>
           <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
@@ -37,69 +35,98 @@ import { Update } from "../Actions/adminActions";
               <table className="min-w-full divide-y divide-gray-300">
                 <thead>
                   <tr>
-                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                    >
                       Name
                     </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
                       Company Name
                     </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
                       Email
                     </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      Proof of Address 
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Proof of Address
                     </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      Proof of identity 
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Proof of identity
                     </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    Tax Register
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Tax Register
                     </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                        Action
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Action
                     </th>
                   </tr>
                 </thead>
-                  
+
                 <tbody className="divide-y divide-gray-200">
                   {users.map((user) => (
-                   
-                      <tr  key={user.id}>
-              
+                    <tr key={user.id}>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
                         {user.name}
                       </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user.companyName}</td>
-                       
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user.email}</td>
-               
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{}</td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{}</td>
-                      
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {user.companyName}
+                      </td>
+
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {user.email}
+                      </td>
+
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {}
+                      </td>
+
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {}
+                      </td>
                       <td className="relative whitespace-nowrap  py-4 pl-3 pr-4 text-sm font-medium sm:pr-0">
-                      <div className="flex gap-4">
-                        <button className="text-indigo-600 hover:text-indigo-900">
-                          Approve<span className="sr-only">, {user.name}</span>
-                        </button>
-                 
-                        <button className="text-indigo-600 hover:text-indigo-900" onClick={Update()}>
-                          Reject<span className="sr-only">, {user.name}</span>
-                        </button>
+                        <div className="flex gap-4">
+                          <button className="text-indigo-600 hover:text-indigo-900">
+                            Approve
+                            <span className="sr-only">, {user.name}</span>
+                          </button>
+
+                          <button
+                            className="text-indigo-600 hover:text-indigo-900"
+                            onClick={Update()}
+                          >
+                            Reject<span className="sr-only">, {user.name}</span>
+                          </button>
                         </div>
                       </td>
-      
-                    </tr> 
-
+                    </tr>
                   ))}
                 </tbody>
-               
               </table>
             </div>
           </div>
         </div>
-        </main>
-  </div>
-  
-  )
-  }
+      </main>
+    </div>
+  );
+}
