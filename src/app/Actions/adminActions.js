7e85da015381517bfe2e310514819719a9f5647e
nourAhmed123 <96887154+nourAhmed123@users.noreply.github.com>
2023-12-24 17:15:47 +0200

    up

--- src/app/Actions/adminActions.js
+++ src/app/Actions/adminActions.js
@@ -16,10 +16,12 @@ export async function UpdateUserStatus(email) {
 export async function GetAdminTableData() {
   try {
     const users = await prisma.user.findMany({
-      include: {
-        company: true,
-      },
+      // include: {
+      //   company: true,
+      // },
     });
+    console.log(users)
+    
     return { users };
   } catch (error) {
     console.error("Error updating user status:", error);
